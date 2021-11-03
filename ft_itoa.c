54c552241419749029cde077d5d16c13b3ffeabe
DivachiLeonerd <81565090+DivachiLeonerd@users.noreply.github.com>
2021-11-03 13:18:44 +0000

    itoa ideia e split talvez mal feito

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,15 +6,42 @@
 /*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 13:24:44 by afonso            #+#    #+#             */
-/*   Updated: 2021/11/01 13:44:34 by afonso           ###   ########.fr       */
+/*   Updated: 2021/11/02 19:47:55 by afonso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_itoa(int n)
 {
-	while (n / 10)
-	{
+	char	ch;
+	char	*string;
+	int		i;
 
+	i = 0;
+	while (n >= 10)
+	{
+		ch = n % 10 + 48;
+		string[i] = ch;
+		n = n / 10;
+		i++;
+	}
+
+}
+
+static	char	*reverse_string(char *string)
+{
+	char	tmp;
+	int		strlen;
+	int		i;
+
+	strlen = 0;
+	while (string[strlen])
+	{
+		strlen++;
+	}
+	strlen++;
+	while (i >= strlen / 2)
+	{
+		
 	}
 
 }
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 10:16:20 by afonso            #+#    #+#             */
-/*   Updated: 2021/11/01 13:23:12 by afonso           ###   ########.fr       */
+/*   Updated: 2021/11/02 16:19:03 by afonso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,58 @@
 
 char	**ft_split(char const *s, char c)
 {
-	unsigned int		counter;
-	unsigned int		i;
-	char				*alloc;
-	char	pointer;
+	int	i;
+	int	string_count;
+	int	stringnum;
+	char	**split;
+	char	**final_array;
 
+	stringnum = 0;
 	i = 0;
-	counter = 0;
+	string_count = 0;
 	while (s[i])
 	{
 		if (s[i] == c)
 		{
-			counter++;
+			stringnum++;
 		}
 		i++;
 	}
-	alloc = malloc((i - counter + 1) * sizeof(char));
-	if (!alloc)
+	stringnum++;//numero de splits mais 1
+	split = malloc((i + 1) * sizeof(char));// i + 1 because of being an index
+	if (!split)
+	{
 		return (NULL);
-		counter = 0;
-	while (s[i])
+	}
+	while (string_count < stringnum && *s)
 	{
-		while (s[i] != c)
+		put_string(s, c, split, string_count);
+		string_count++;
+		if (*put_string(s, c, split, string_count) == '\0')
 		{
-		alloc[counter] = s[i];
-		counter++;
-		i++;
-		}
-		if (s[i] == c)
-		{
-			i++;
+			string_count++;
 		}
 	}
-	alloc[counter] == '\0';
-	pointer = alloc;
-	free(alloc);
-	return (pointer);
+	final_array = split;
+	free(split);
+}
+static char *put_string(char const *s, char c, char **split, int split_index)
+{
+	int		size;
+	char	*string;
+
+	size = 0;
+	while (*s != c && *s)
+	{
+		string[size] = *s;
+		size++;
+		s++;
+	}
+	string[size] = '\0';
+	if (*s != '\0')
+	{
+		s++;
+	}
+	split[split_index] = string;
+	return (s);
 }
