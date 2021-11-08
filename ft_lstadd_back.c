154ee757f2894a6145e9956d4e6c114ed2942224
DivachiLeonerd <81565090+DivachiLeonerd@users.noreply.github.com>
2021-11-08 12:23:40 +0000

    comecei a fazer as bónus e alterei o libft

---
+++ ft_lstadd_back.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 11:53:36 by afonso            #+#    #+#             */
+/*   Updated: 2021/11/08 11:59:24 by afonso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last_position;
+
+	last_position = ft_lstlast(lst[0]);
+	last_position->next = new;
+}
---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afonso <afonso@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 10:11:03 by afonso            #+#    #+#             */
+/*   Updated: 2021/11/08 11:03:34 by afonso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	void	*_content;
+	int		i;
+	char	*parameter;
+	t_list	new_list;
+
+	parameter = (char *)content;
+	i = 0;
+	_content = malloc(sizeof(content));
+	if (_content)
+	{
+		return (NULL);
+	}
+	while (parameter[i])
+	{
+		((char *)_content)[i] = parameter[i];
+		i++;
+	}
+	new_list.content = _content;
+	new_list.next = NULL;
+	return ((t_list *)_content);
+}
